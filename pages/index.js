55f3e390ef49e9c6c1a739ca9fd3c7fdb24d69fe
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-07 16:03:09 -0300

    feat: using navigation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+import Link from 'next/link'
+
 function HomePage() {
     console.log("[HomePage] render")
     return (
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <l1>
+                        <Link href="/about">About</Link>
+                    </l1>
+                </ul>
+            </nav>
+        </header>
         <main>
             <h1>My Blog</h1>
         </main>
+        </>
     )
 }
 
