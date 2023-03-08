d7dd1febf4bdb7a5224a59d86d04da16763c0bf9
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-08 14:55:16 -0300

    style: using style-jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <nav>
+        <nav className="navBar">
             <ul>
                 <li>
                     <Link href="/">Home</Link>
@@ -11,6 +11,21 @@ export default function NavBar() {
                     <Link href="/about">About</Link>
                 </li>
             </ul>
+            <style jsx>{`
+                ul {
+                    list-style: none;
+                    padding: 0
+                }
+                
+                li {
+                    display: inline;
+                }
+                
+                li:not(:first-child) {
+                    margin-left: 0.75rem;
+                }
+                
+            `}</style>
         </nav>
     )
 }
