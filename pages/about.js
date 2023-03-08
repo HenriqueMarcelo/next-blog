48ea39997f61fff9ad7f221b1f28d6bfa546ff17
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-08 10:21:03 -0300

    feat: custom App component

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,8 @@
-import NavBar from "../components/NavBar"
 
 export default function AboutPage(){
-    console.log('[AboutPage] render')
     return (
-        <>
-            <header>
-                <NavBar />
-            </header>
-            <main>
-                <h1>About</h1>
-            </main>
-        </>
+        <main>
+            <h1>About</h1>
+        </main>
     )
 }
