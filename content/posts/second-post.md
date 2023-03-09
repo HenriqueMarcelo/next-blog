76c1ac84ff4fc10ee6003957375eb8178cf8e6be
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-09 10:37:16 -0300

    feat: dynamic routes

---
+++ content/posts/second-post.md
@@ -0,0 +1,15 @@
+---
+title: "Second Post"
+date: "2023-03-09"
+---
+
+This is my Second post, written in Markdown.
+
+Here's some __bold text__.
+
+And some list:
+
+- Alfa
+- Bravo
+- Charlie
+- Delta
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,8 +2,18 @@
 import Head from "next/head"
 import { getPost } from "../../iib/posts"
 
-export async function getStaticProps() {
-    const post = await getPost('first-post')
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { slug: 'first-post' } },
+            { params: { slug: 'second-post' } },
+        ],
+        fallback: false,
+    }
+}
+
+export async function getStaticProps({params: {slug}}) {
+    const post = await getPost(slug)
     return {
         props: {
             post
@@ -11,7 +21,7 @@ export async function getStaticProps() {
     }
 }
 
-export default function FirstPost({post}){
+export default function PostPage({post}){
     return(
         <main>
             <Head>
