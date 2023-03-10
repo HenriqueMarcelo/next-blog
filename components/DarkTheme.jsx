c85123a97d941bafc6707db5194be7dde3515684
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-03-10 10:38:29 -0300

    feat: DarkTheme component

---
+++ components/DarkTheme.jsx
@@ -0,0 +1,12 @@
+export default function DarkTheme() {
+    return (
+        <style jsx global>{`
+            /*Dark theme*/
+            :root{
+                --background-color: #121212;
+                --link-color: gold;
+                --text-color: floralwhite;
+            }
+        `}</style>
+    )
+}
--- components/ThemeSwitch.jsx
+++ components/ThemeSwitch.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import DarkTheme from "./DarkTheme";
 
 export function ThemeSwitch() {
     const [darkMode, setDarkMode] = useState(false);
@@ -13,11 +14,12 @@ export function ThemeSwitch() {
                     background: none;
                     border: none;
                     cursor: pointer;
-
+                    color: inherit;
                     padding: 0.5rem 1rem;
-                    border: 2px solid black;
+                    border: 2px solid var(--text-color);
                 }
             `}</style>
+            { darkMode && <DarkTheme /> }
         </>
     )
 }
